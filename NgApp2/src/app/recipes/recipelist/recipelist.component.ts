a7aab1eaf097c20eef7b1b41cc8aca524ecce876
louhack <loic.haquin@gmail.com>
2017-11-09 21:16:22 +0100

    routing for new and edit recipes

--- NgApp2/src/app/recipes/recipelist/recipelist.component.ts
+++ NgApp2/src/app/recipes/recipelist/recipelist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-recipelist',
@@ -10,11 +11,16 @@ import { RecipeService } from "../recipe.service";
 export class RecipelistComponent implements OnInit {
     recipeItems: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) { }
+    constructor(private recipeService: RecipeService,
+      private router: Router,
+      private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipeItems = this.recipeService.getRecipes();
   }
 
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
 
 }
--- NgApp2/src/app/recipes/recipes-details/recipes-details.component.ts
+++ NgApp2/src/app/recipes/recipes-details/recipes-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from "../recipe.service";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-recipes-details',
@@ -15,7 +15,8 @@ export class RecipesDetailsComponent implements OnInit {
     id: number;
 
     constructor(private recipeService: RecipeService,
-      private route: ActivatedRoute) {
+      private route: ActivatedRoute,
+      private router: Router) {
 
     }
 
@@ -31,6 +32,12 @@ export class RecipesDetailsComponent implements OnInit {
       // console.log("Ingredients " + this.recipe.ingredients);
       this.recipeService.addIngredientsToSL(this.recipe.ingredients);
       this.addedToSl = true;
+
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+    //this.router.navigate(['../', this.id,'edit'], { relativeTo: this.route });  // for demo purpose
 
   }
 

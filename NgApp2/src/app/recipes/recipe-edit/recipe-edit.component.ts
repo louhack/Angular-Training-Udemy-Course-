9212e7903b8b84334fc0b29d95d59bc93c549c8f
louhack <loic.haquin@gmail.com>
2017-11-19 19:56:11 +0100

    remove ingredients from a recipe

--- NgApp2/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ NgApp2/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -95,4 +95,8 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
 }

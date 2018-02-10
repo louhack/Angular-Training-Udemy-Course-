3351f6f7cafae93cad506b537da6663d7b3b9de7
louhack <loic.haquin@gmail.com>
2018-02-10 18:38:34 +0100

    Authentication : Sign Up

--- NgApp2/src/app/app.component.ts
+++ NgApp2/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDWy5yqH3Rxd7DvVJ_6_nQK1BT_yEStzKU',
+      authDomain: 'ng-recipebook-shoppinglist.firebaseapp.com'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
     // componentDisplayed: String = 'Recipes';
 
     // componentToShow(linkCliked: String) {
--- NgApp2/src/app/auth/signup/signup.component.ts
+++ NgApp2/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,14 +10,17 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+
   }
 
   onSignup(form: NgForm) {
     const email = form.value.email;
-    const password = form.value.email;
+    const password = form.value.password;
+    console.log (form);
+    this.authService.signupUser(email, password);
   }
 
 }
